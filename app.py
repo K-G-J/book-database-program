5c80bf9f741a0d4ef78cc1153933957bc1a16664
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-20 15:52:11 -0500

    added our database model

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+from models import (Base, session, Book, engine)
+
 # Import models
 # Main menu - add, search, analysis, exit, view
 # Add books to the database
-# Edit books 
+# Edit books
 # Delete books
 # Search books
-# Data cleaning 
+# Data cleaning
 # Loop runs program
+
+if __name__ == '__main__':
+    # Create the database
+    Base.metadata.create_all(engine)
